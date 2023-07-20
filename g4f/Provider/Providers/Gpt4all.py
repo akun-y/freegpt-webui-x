4840dc3db1ac2426df534ee0d758840a803654a7
akun <akun.win2022>
2023-07-20 18:23:53 +0800

    modified:   g4f/Provider/Providers/Gpt4all.py

--- g4f/Provider/Providers/Gpt4all.py
+++ g4f/Provider/Providers/Gpt4all.py
@@ -5,6 +5,7 @@ import os
 import json
 import requests
 from ...typing import sha256, Dict, get_type_hints
+from gpt4all import GPT4All
 
 url = 'https://gpt-gm.h2o.ai'
 model = ['gpt4all-7b']
@@ -18,96 +19,63 @@ models = {
     'llama-13b': 'h2oai/h2ogpt-gm-oasst1-en-2048-open-llama-13b'
 }
 
+gpt4allModel = GPT4All(
+        # model_name="wizardLM-13B-Uncensored.ggmlv3.q4_0",
+        # model_name="orca-mini-13b.ggmlv3.q4_0",
+        # model_name="GPT4All-13B-snoozy.ggmlv3.q4_0",
+        # model_name="nous-hermes-13b.ggmlv3.q4_0",
+         model_name="ggml-gpt4all-j-v1.3-groovy.bin",
+        # model_name="orca-mini-7b.ggmlv3.q4_0.bin",
+        # model_name='orca-mini-3b.ggmlv3.q4_0.bin',
+        # model_path='/mnt/k/tmp/GPT4All-models',
+        model_path='K:/dev/AI/GPT4All-models',
+        allow_download=False)
 def _create_completion(model: str, messages: list, stream: bool, **kwargs):
-    conversation =''
+    global gpt4allModel 
+    conversation = ''
     #conversation = 'instruction: this is a conversation beween, a user and an AI assistant, respond to the latest message, referring to the conversation if needed\n'
     for message in messages:
         conversation += '%s: %s\n' % (message['role'], message['content'])
     conversation += 'assistant:'
-    
-    client = Session()
-    client.headers = {
-        'authority': 'gpt-gm.h2o.ai',
-        'origin': 'https://gpt-gm.h2o.ai',
-        'referer': 'https://gpt-gm.h2o.ai/',
-        'sec-ch-ua': '"Not.A/Brand";v="8", "Chromium";v="114", "Google Chrome";v="114"',
-        'sec-ch-ua-mobile': '?0',
-        'sec-ch-ua-platform': '"Windows"',
-        'sec-fetch-dest': 'document',
-        'sec-fetch-mode': 'navigate',
-        'sec-fetch-site': 'same-origin',
-        'sec-fetch-user': '?1',
-        'upgrade-insecure-requests': '1',
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
-    }
 
-    client.get('https://gpt-gm.h2o.ai/')
-    response = client.post('https://gpt-gm.h2o.ai/settings', data={
-        'ethicsModalAccepted': 'true',
-        'shareConversationsWithModelAuthors': 'true',
-        'ethicsModalAcceptedAt': '',
-        'activeModel': 'h2oai/h2ogpt-gm-oasst1-en-2048-falcon-40b-v1',
-        'searchEnabled': 'true',
-    })
+    # gpt4allModel = GPT4All(
+    #     #model_name="wizardLM-13B-Uncensored.ggmlv3.q4_0",
+    #     # model_name="orca-mini-13b.ggmlv3.q4_0",
+    #     model_name="GPT4All-13B-snoozy.ggmlv3.q4_0",
+    #     # model_name="nous-hermes-13b.ggmlv3.q4_0",
+    #     #model_name="nous-hermes-13b.ggmlv3.q4_0.bin",
+    #     #model_name="ggml-gpt4all-j-v1.3-groovy.bin",
+    #     # model_name="ggml-model-gpt4all-falcon-q4_0.bin",
+    #     #model_name="orca-mini-7b.ggmlv3.q4_0.bin",
+    #     #model_name='orca-mini-3b.ggmlv3.q4_0.bin',
+    #     #model_path='/mnt/k/tmp/GPT4All-models',
+    #     model_path='K:/dev/AI/GPT4All-models',
+    #     allow_download=False)
 
-    headers = {
-        'authority': 'gpt-gm.h2o.ai',
-        'accept': '*/*',
-        'accept-language': 'en,fr-FR;q=0.9,fr;q=0.8,es-ES;q=0.7,es;q=0.6,en-US;q=0.5,am;q=0.4,de;q=0.3',
-        'origin': 'https://gpt-gm.h2o.ai',
-        'referer': 'https://gpt-gm.h2o.ai/',
-        'sec-ch-ua': '"Not.A/Brand";v="8", "Chromium";v="114", "Google Chrome";v="114"',
-        'sec-ch-ua-mobile': '?0',
-        'sec-ch-ua-platform': '"Windows"',
-        'sec-fetch-dest': 'empty',
-        'sec-fetch-mode': 'cors',
-        'sec-fetch-site': 'same-origin',
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
-    }
+    output = gpt4allModel.generate(message['content'],max_tokens=30)
+    print("GPT4ALL output:",output)
 
-    json_data = {
-        'model': models[model]
-    }
+    conversationId ="234234"
 
-    response = client.post('https://gpt-gm.h2o.ai/conversation',
-                            headers=headers, json=json_data)
-    conversationId = response.json()['conversationId']
-
-
-    completion = client.post(f'https://gpt-gm.h2o.ai/conversation/{conversationId}', stream=True, json = {
-        'inputs': conversation,
-        'parameters': {
-            'temperature': kwargs.get('temperature', 0.4),
-            'truncate': kwargs.get('truncate', 2048),
-            'max_new_tokens': kwargs.get('max_new_tokens', 1024),
-            'do_sample': kwargs.get('do_sample', True),
-            'repetition_penalty': kwargs.get('repetition_penalty', 1.2),
-            'return_full_text': kwargs.get('return_full_text', False)
-        },
-        'stream': True,
-        'options': {
-            'id': kwargs.get('id', str(uuid4())),
-            'response_id': kwargs.get('response_id', str(uuid4())),
-            'is_retry': False,
-            'use_cache': False,
-            'web_search_id': ''
-        }
-    })
-    if not completion.ok:
-        error_msg = completion.reason + ': ' + completion.text
+    if not output or len(output) == 0:
+        error_msg = "response is empty"
         print("conversation completion error:"+error_msg)
-        error = {'error': completion.reason}
+        error = {'error':error_msg}
         yield error
         return
-    for line in completion.iter_lines():
-        if b'data' in line:
-            line = loads(line.decode('utf-8').replace('data:', ''))
-            token = line['token']['text']
-            
-            if token == '<|endoftext|>':
-                break
-            else:
-                yield (token)
-            
+    token  = output;
+    yield token
+    # for line in completion.iter_lines():
+    #     if b'data' in line:
+    #         line = loads(line.decode('utf-8').replace('data:', ''))
+    #         token = line['token']['text']
+
+    #         if token == '<|endoftext|>':
+    #             break
+    #         else:
+    #             yield (token)
+
+
 params = f'g4f.Providers.{os.path.basename(__file__)[:-3]} supports: ' + \
-    '(%s)' % ', '.join([f"{name}: {get_type_hints(_create_completion)[name].__name__}" for name in _create_completion.__code__.co_varnames[:_create_completion.__code__.co_argcount]])
+    '(%s)' % ', '.join(
+        [f"{name}: {get_type_hints(_create_completion)[name].__name__}" for name in _create_completion.__code__.co_varnames[:_create_completion.__code__.co_argcount]])
